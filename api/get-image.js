63c530c5f0f01ea405a83582d59007a99d4975c4
gipicihuy <154876480+gipicihuy@users.noreply.github.com>
2025-09-18 16:32:40 +0700

    Update get-image.js

--- api/get-image.js
+++ api/get-image.js
@@ -11,35 +11,25 @@ export default async function handler(req, res) {
         return res.status(400).send('URL is required.');
     }
 
-    let absoluteUrl = url;
-
-    // Tambahkan 'https://' jika URL tidak memiliki protokol
-    if (!absoluteUrl.startsWith('http://') && !absoluteUrl.startsWith('https://')) {
-        absoluteUrl = `https://${absoluteUrl}`;
-    }
-
     try {
-        const response = await fetch(absoluteUrl, {
-            // Menambahkan user-agent untuk menyamar sebagai browser
+        const response = await fetch(url, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 'Referer': 'https://www.tiktok.com/'
             }
         });
 
-        // Periksa apakah respons berhasil
         if (!response.ok) {
-            console.error(`Failed to fetch image: ${response.statusText} for URL: ${absoluteUrl}`);
+            console.error(`Failed to fetch image: ${response.statusText} for URL: ${url}`);
             return res.status(response.status).send(`Failed to fetch image: ${response.statusText}`);
         }
 
         const contentType = response.headers.get('content-type');
         if (contentType && contentType.startsWith('image/')) {
             res.setHeader('Content-Type', contentType);
-            // Salurkan data gambar langsung ke respons
             response.body.pipe(res);
         } else {
-            console.error(`Unexpected content type: ${contentType} for URL: ${absoluteUrl}`);
+            console.error(`Unexpected content type: ${contentType} for URL: ${url}`);
             res.status(400).send('URL does not point to an image.');
         }
 
